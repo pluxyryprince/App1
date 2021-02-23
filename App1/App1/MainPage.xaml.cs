addc8e97bb327452c45c337dbb96bf636b41ec75
Prince Pluxury <grisha.kluchka404@gmail.com>
2021-02-23 18:13:30 +0300

    ну не совсем, но готово

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Label label = new Label
+            {
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = "Tap to count ++",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            };
+
+            TapGestureRecognizer tapGesture = new TapGestureRecognizer
+            {
+                NumberOfTapsRequired = 1
+            };
+
+            int count = 0;  // счетчик нажатий
+            tapGesture.Tapped += (s, e) =>
+            {
+                count++;
+                if (count % 2 == 0)
+                {
+                    label.BackgroundColor = Color.Black;
+                    label.TextColor = Color.White;
+                }
+
+                else
+                {
+                    label.TextColor = Color.Black;
+                    label.BackgroundColor = Color.Lavender;
+                }
+                label.Text = $"Count of students : {count}";
+            };
+
+            label.GestureRecognizers.Add(tapGesture);
+
+            Content = label;
+
+        }
+            
+    }
+}
+
+
+    
